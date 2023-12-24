f0f9a4fc04cf4af0bc4d7414c26570072b70d27f
Hemi <sagerlawgeli@gmail.com>
2023-12-24 20:27:46 +0100

    Removes glass card from login/reg

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,7 +22,7 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-sm shadow- 00 overflow-hidden sm:rounded-lg card glass">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-sm shadow- 00 overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
